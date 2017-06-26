04b9d101edf957f7b0ff6197d21abc5c8fad2068
Andreas Schneider <asn@cryptomilk.org>
2017-06-26 09:02:16 +0200

    Big bang!
    This adds the boiler plate for the plugin from http://wppb.io/

---
+++ climbing-concepts.php
@@ -0,0 +1,75 @@
+<?php
+
+/**
+ * The plugin bootstrap file
+ *
+ * This file is read by WordPress to generate the plugin information in the plugin
+ * admin area. This file also includes all of the dependencies used by the plugin,
+ * registers the activation and deactivation functions, and defines a function
+ * that starts the plugin.
+ *
+ * @link              http://example.com
+ * @since             1.0.0
+ * @package           ClimbingConcepts
+ *
+ * @wordpress-plugin
+ * Plugin Name:       Climbing Concepts
+ * Plugin URI:        https://github.com/cryptomilk/climbing-concepts
+ * Description:       This is a plugin to manage temporary climbing restrictions for rocks/walls.
+ * Version:           0.0.1
+ * Author:            Andreas Schneider
+ * Author URI:        https://cryptomilk.org/
+ * License:           GPL-3.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-3.0.txt
+ * Text Domain:       climbing-concepts
+ * Domain Path:       /languages
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+    die;
+}
+
+/**
+ * The code that runs during plugin activation.
+ * This action is documented in includes/class-climbing-concepts-activator.php
+ */
+function activate_plugin_name() {
+    require_once plugin_dir_path( __FILE__ ) . 'includes/class-climbing-concepts-activator.php';
+    ClimbingConcepts_Activator::activate();
+}
+
+/**
+ * The code that runs during plugin deactivation.
+ * This action is documented in includes/class-climbing-concepts-deactivator.php
+ */
+function deactivate_plugin_name() {
+    require_once plugin_dir_path( __FILE__ ) . 'includes/class-climbing-concepts-deactivator.php';
+    ClimbingConcepts_Deactivator::deactivate();
+}
+
+register_activation_hook( __FILE__, 'activate_plugin_name' );
+register_deactivation_hook( __FILE__, 'deactivate_plugin_name' );
+
+/**
+ * The core plugin class that is used to define internationalization,
+ * admin-specific hooks, and public-facing site hooks.
+ */
+require plugin_dir_path( __FILE__ ) . 'includes/class-climbing-concepts.php';
+
+/**
+ * Begins execution of the plugin.
+ *
+ * Since everything within the plugin is registered via hooks,
+ * then kicking off the plugin from this point in the file does
+ * not affect the page life cycle.
+ *
+ * @since    1.0.0
+ */
+function run_plugin_name() {
+
+    $plugin = new ClimbingConcepts();
+    $plugin->run();
+
+}
+run_plugin_name();
---
+++ includes/class-climbing-concepts-i18n.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * Define the internationalization functionality
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @link       http://example.com
+ * @since      1.0.0
+ *
+ * @package    ClimbingConcepts
+ * @subpackage ClimbingConcepts/includes
+ */
+
+/**
+ * Define the internationalization functionality.
+ *
+ * Loads and defines the internationalization files for this plugin
+ * so that it is ready for translation.
+ *
+ * @since      1.0.0
+ * @package    ClimbingConcepts
+ * @subpackage ClimbingConcepts/includes
+ * @author     Your Name <email@example.com>
+ */
+class ClimbingConcepts_i18n {
+    /**
+     * Load the plugin text domain for translation.
+     *
+     * @since    1.0.0
+     */
+    public function load_plugin_textdomain() {
+
+        load_plugin_textdomain('climbing-concepts',
+                               false,
+                               dirname( dirname( plugin_basename( __FILE__ ) ) ) . '/languages/');
+
+    }
+}
